06cee36f294cfb695b61c7d67e8f454c42046f7c
Prakash Bist <prakash.bist@smartsolar.com.np>
2023-08-24 10:15:42 +0900

    Initial Commit

---
+++ MetaMessenger/MetaMessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaMessengerApp.swift
+//  MetaMessenger
+//
+//  Created by Prakash Bist on 2023/08/24.
+//
+
+import SwiftUI
+
+@main
+struct MetaMessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
